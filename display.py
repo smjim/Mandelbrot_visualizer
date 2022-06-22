4b5008b2d94d5907cf6c9dc08921af1020d7c06a
smjim <jamesrogers2507@gmail.com>
2022-06-22 00:14:47 -0400

    implement derbail set-checking

--- display.py
+++ display.py
@@ -3,7 +3,9 @@ import pylab as plt
 
 data = np.loadtxt("data.txt", dtype=int)
 
-fig, ax2 = plt.subplots()
-ax2.imshow(data, extent=[-2.00,0.47,-1.12,1.12])
+plt.imshow(data, cmap = 'plasma', origin = 'lower',  extent = [-2.00,0.47,-1.12,1.12])
+plt.xlabel('Real')
+plt.ylabel('Imaginary')
 
+plt.colorbar();
 plt.show()
--- generate_set.cpp
+++ generate_set.cpp
@@ -3,62 +3,83 @@
 
 using namespace std;
 
-class Complex {
-	public:
-		float real, imag;
-		int belongs = 0;
-		Complex(float r = 0, float i = 0) {real = r;	imag = i;}
-		 
-		Complex operator + (Complex const &obj) {
-			Complex res;
-			res.real = real + obj.real;
-			res.imag = imag + obj.imag;
-			return res;
-		}
-
-		void square() {
-			float tmp = real;
-			real = real*real - imag*imag;
-			imag = 2*tmp*imag;
-		}
-
-		void testPoint(int max_iterations) {
-			// z_n+1 = (z_n)^2 + c | z_0 = 0
-			Complex z(0, 0);
-
-			// breakout condition is that magnitude of point is <= 2
-			while (z.real*z.real + z.imag*z.imag <= 4. && belongs < max_iterations) {	
-				z.square();
-				z.real += real;
-				z.imag += imag;
-				belongs ++;
-			}
-		}
-
-		void print() { cout << real << " + i" << imag << ": "; }
-};
-
-void naive() {
+void optimized(int max_iteration) {
 	int res = 1000; // resolution of the set boundary
 	
 	ofstream data;
 	data.open ("data.txt");
 	for (int j = 0; j < res; j++) {
 		for (int i = 0; i < res; i++) {
-			float x = (i * 2.47/res) - 2.00; // Scaled to lie in Mandelbrot X scale (-2.00, 0.47)
-			float y = (j * 2.24/res) - 1.12; // Scaled to lie in Mandelbrot Y scale (-1.12, 1.12)
+			float x0 = (i * 2.47/res) - 2.00; // Scaled to lie in Mandelbrot X scale (-2.00, 0.47)
+			float y0 = (j * 2.24/res) - 1.12; // Scaled to lie in Mandelbrot Y scale (-1.12, 1.12)
 
-			Complex point(x, y);
-			point.testPoint(100);
-			//point.print();
-			data << point.belongs << " ";
+			float x1 = 0;
+			float y1 = 0;
+			float x2 = 0;
+			float y2 = 0;
+			int iteration = 0;
+
+			// z(n+1) = z(n)^2 + c
+			while (x2 + y2 <= 4 && iteration < max_iteration) {
+				y1 = 2*x1*y1 + y0;
+				x1 = x2 - y2 + x0;
+
+				x2 = x1*x1;
+				y2 = y1*y1;
+				iteration ++;
+			}
+			data << iteration << " ";
+		}
+		data << endl;
+	}
+}
+
+void derbail(int max_iteration) {
+	int res = 1000; // resolution of the set boundary
+	
+	ofstream data;
+	data.open ("data.txt");
+	for (int j = 0; j < res; j++) {
+		for (int i = 0; i < res; i++) {
+			float x0 = (i * 2.47/res) - 2.00; // Scaled to lie in Mandelbrot X scale (-2.00, 0.47)
+			float y0 = (j * 2.24/res) - 1.12; // Scaled to lie in Mandelbrot Y scale (-1.12, 1.12)
+
+			float x1 = 0;
+			float y1 = 0;
+
+			float dx = 1;
+			float dy = 0;
+			float dx_sum = 0;
+			float dy_sum = 0;
+
+			int iteration = 0;
+
+			int dbail = 1e6;
+
+			// z'(n+1) = 2 * z'(n) * z(n) + 1
+			while (dx_sum*dx_sum + dy_sum*dy_sum < dbail && x1*x1 + y1*y1 <= 4 && iteration < max_iteration) {
+				float xtmp = x1*x1 - y1*y1 + x0;
+				y1 = 2*x1*y1 + y0;
+				x1 = xtmp;
+
+				float dxtmp = 2*(dx*x1 - dy*y1) + 1;
+				dy = 2*(dy*x1 + dx*y1);
+				dx = dxtmp;
+
+				dx_sum += dx;
+				dy_sum += dy;
+
+				iteration ++;
+			}
+			data << iteration << " ";
 		}
 		data << endl;
 	}
 }
 
 int main() {
-	naive();
+	//optimized(20);
+	derbail(20);
 
 	return 0;
 }
